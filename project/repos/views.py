3330bcaa1f21ecba1184555f4b7de2305ac1efcd
Kaushik ASP <aspkaushik@gmail.com>
2018-09-07 13:07:00 +0530

    everything working

--- project/repos/views.py
+++ project/repos/views.py
@@ -15,7 +15,21 @@ repo_names = {}
 def utility_processor():
     def convert_text_to_html(string):
         return textile(string)
-    return dict(convert_text_to_html=convert_text_to_html)
+
+    def get_fragment_list(path):
+        path_fragment_list = list(filter(None, path.split('/')))[3:]
+        urls = []
+        for index in range(len(path_fragment_list)):
+            url_path = '/'.join(path_fragment_list[:index+1])
+            url_name = path_fragment_list[index]
+            urls.append(dict(url_path=url_path, url_name=url_name))
+        return urls
+
+    return dict(
+                convert_text_to_html=convert_text_to_html,
+                get_fragment_list=get_fragment_list
+                )
+
 
 def get_repo(owner_name, repo_name):
     full_repo_name = "{}/{}".format(owner_name, repo_name)
@@ -37,7 +51,7 @@ def get_repo_structure(owner_name, repo_name):
     repo_contents = get_contents('/', repo)
     # print(repo_contents)
     # repo_content = get_repo_content(repo_name="{}/{}".format(owner_name, repo_name))
-    return render_template('./repos/index.html', repo_contents=repo_contents)
+    return render_template('./repos/index.html', repo_contents=repo_contents, repo=repo)
 
 @repos_blueprint.route('/<owner_name>/<repo_name>/<path:dir_name>/', defaults={'file_name': None})
 @repos_blueprint.route('/<owner_name>/<repo_name>/<path:dir_name>/<file_name>')
@@ -51,7 +65,7 @@ def get_dir(owner_name, repo_name, dir_name, file_name=None):
     repo = get_repo(owner_name, repo_name)
     repo_contents = get_contents(path, repo)
     # print(repo_contents)
-    return render_template('./repos/index.html', repo_contents=repo_contents)
+    return render_template('./repos/index.html', repo_contents=repo_contents, repo=repo)
 
 
 
